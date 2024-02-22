b6cf13ce498a8bc0e7351e49502fa40b2a4cb267
Solractys <135873875+Solractys@users.noreply.github.com>
2024-02-22 05:21:44 -0300

    test

--- backtrack.py
+++ backtrack.py
@@ -1,13 +1,13 @@
 def permute(nums): 
-    ans = []
+    inp = []
     k = len(nums)
     def backtrack(start, arr):
         if len(arr)==k:                
-            ans.append(arr.copy())
+            inp.append(arr.copy())
         for i in range(0,len(nums)):
             if nums[i] not in arr:
                 arr.append(nums[i])
                 backtrack(i+1, arr)
                 arr.pop()
     backtrack(0, [])
-    return ans
+    return inp
